78988e3881102e90db5270884783f566eef1ccb7
Vikki Chan <vikkiwchan@gmail.com>
2021-03-10 09:41:52 -0600

    growing rows complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import store from "../store";
+import React, { Component } from 'react';
+import { store, addRow } from '../store';
 
 class App extends Component {
   constructor() {
@@ -17,29 +17,31 @@ class App extends Component {
   render() {
     const { grid } = this.state;
     return (
-      <div id="pixelate">
+      <div id='pixelate'>
         <h1>Pixelate</h1>
         <div>
-          <button id="add-row">Add a row</button>
+          <button id='add-row' onClick={() => store.dispatch(addRow())}>
+            Add a row
+          </button>
           <select>
-            <option value="red">Red</option>
-            <option value="orange">Orange</option>
-            <option value="yellow">Yellow</option>
-            <option value="green">Green</option>
-            <option value="blue">Blue</option>
-            <option value="indigo">Indigo</option>
-            <option value="violet">Violet</option>
-            <option value="black">Black</option>
-            <option value="white">White</option>
-            <option value="brown">Brown</option>
+            <option value='red'>Red</option>
+            <option value='orange'>Orange</option>
+            <option value='yellow'>Yellow</option>
+            <option value='green'>Green</option>
+            <option value='blue'>Blue</option>
+            <option value='indigo'>Indigo</option>
+            <option value='violet'>Violet</option>
+            <option value='black'>Black</option>
+            <option value='white'>White</option>
+            <option value='brown'>Brown</option>
           </select>
         </div>
         <table>
           <tbody>
             {grid.map((row, idx) => (
               <tr key={`${row}-${idx}`}>
-                {row.map((cell, idx) => (
-                  <td key={`${cell}-${idx}`}>{cell}</td>
+                {row.map((color, idx) => (
+                  <td key={`${color}-${idx}`} className={color}></td>
                 ))}
               </tr>
             ))}
--- src/store.js
+++ src/store.js
@@ -4,23 +4,36 @@
 // Much like requests in express pass from middleware to middleware, actions in redux
 // pass from middleware to middleware. The loggerMiddleware gets a chance to see
 // actions before they are processed. They get in the middle, hence, middleware.
-import { createStore, applyMiddleware } from "redux";
-import loggerMiddleware from "redux-logger";
+import { createStore, applyMiddleware } from 'redux';
+import loggerMiddleware from 'redux-logger';
 
 // We'll soon revisit the initial state of this application.
 const initialState = {
-  grid: [Array(20).fill("")],
+  grid: [Array(20).fill('')],
 };
 
 // ACTION TYPES
 /* we'll add some action types soon */
+const ADD_ROW = 'ADD_ROW';
+const SELECT_COLOR = 'SELECT_COLOR';
 
 // ACTION CREATORS
 /* we'll also add the corresponding action creators */
+const addRow = () => {
+  return {
+    type: ADD_ROW,
+    row: Array(20).fill(''),
+  };
+};
 
 // And we'll revisit this reducer.
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_ROW:
+      return {
+        ...state,
+        grid: [...state.grid, action.row],
+      };
     default:
       return state;
   }
@@ -28,4 +41,4 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer, applyMiddleware(loggerMiddleware));
 
-export default store;
+export { store, addRow };
