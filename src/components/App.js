30589601450b5b6fbb4af9e0b603802731a0456e
Vikki Chan <vikkiwchan@gmail.com>
2021-03-09 19:56:16 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div id='pixelate'>
+        <h1>Pixelate</h1>
+        <div>
+          <button id='add-row'>Add a row</button>
+          <select>
+            <option value='red'>Red</option>
+            <option value='orange'>Orange</option>
+            <option value='yellow'>Yellow</option>
+            <option value='green'>Green</option>
+            <option value='blue'>Blue</option>
+            <option value='indigo'>Indigo</option>
+            <option value='violet'>Violet</option>
+            <option value='black'>Black</option>
+            <option value='white'>White</option>
+            <option value='brown'>Brown</option>
+          </select>
+        </div>
+        <table></table>
+      </div>
+    );
+  }
+}
+
+export default App;
